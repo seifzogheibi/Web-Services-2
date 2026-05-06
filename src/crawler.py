48036f48f212680e36e55a9dfb91712d0e386cd5
seifzogheibi <fy22smts@leeds.ac.uk>
2026-05-06 10:57:23 +0100

    added  text extraction

--- src/crawler.py
+++ src/crawler.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def fetch_page(url):
@@ -10,3 +11,31 @@ def fetch_page(url):
     response = requests.get(url, timeout=10)
     response.raise_for_status()
     return response.text
+
+
+def extract_page_text(html):
+    """
+    Extract searchable text from a quotes.toscrape.com page.
+
+    This includes quote text, author names, and tags.
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    text_parts = []
+
+    quotes = soup.find_all("div", class_="quote")
+
+    for quote in quotes:
+        quote_text = quote.find("span", class_="text")
+        author = quote.find("small", class_="author")
+        tags = quote.find_all("a", class_="tag")
+
+        if quote_text is not None:
+            text_parts.append(quote_text.get_text(" ", strip=True))
+
+        if author is not None:
+            text_parts.append(author.get_text(" ", strip=True))
+
+        for tag in tags:
+            text_parts.append(tag.get_text(" ", strip=True))
+
+    return " ".join(text_parts)
